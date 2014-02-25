17f6e9e6e8fcc6bb0c2df72416785957bf49810a
Gerry Said <gerrysaid@gmail.com>
2014-02-25 12:10:27 +0100

    Initial commit
    Initial commit

---
+++ SideScroller/Assets/Scripts/laserController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var laserSpeed:int;
+
+
+function Start () {
+
+}
+
+function Update () {
+	//move the laser FORWARD
+	transform.Translate(Vector3.right *
+		laserSpeed * Time.deltaTime);
+}
+
+function OnBecameInvisible()
+{
+	Destroy(this.gameObject);
+}
---
+++ SideScroller/Assets/Scripts/rocketshipController.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+var laser:Rigidbody;
+
+function Start () {
+
+}
+
+function Update () {
+	//mouse control of rocket ship
+	transform.position.y = 
+	Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+	//code which shoots laser on left mouse click
+	if (Input.GetMouseButtonDown(0))
+	{
+	 //create a laser in the spaceship's position
+	 Instantiate(laser,transform.position,Quaternion.identity);
+	}
+}
