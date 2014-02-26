a2ab7afcda022aa07d558bf4fae65bbc2bbda0fd
Gerry Said <gerrysaid@gmail.com>
2014-02-26 09:06:03 +0100

    Infinite scrolling background
    Added infinite scrolling background

---
+++ SideScroller/Assets/Scripts/backgroundVisible.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+
+function OnBecameInvisible()
+{
+	transform.position.x = Camera.main.ScreenToWorldPoint(Vector3(Screen.width,0,0)).x;
+	
+}
---
+++ SideScroller/Assets/Scripts/cameraController.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var cameraspeed:int;
+
+
+
+function Start () {
+
+}
+
+function Update () {
+	transform.Translate(Vector3.right * cameraspeed * Time.deltaTime);
+}
